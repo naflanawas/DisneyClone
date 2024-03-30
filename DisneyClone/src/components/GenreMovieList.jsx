21bc9330255eb0644f22b4875327a3039ffeb090
naflanawas <nafla.20220352@iit.ac.lk>
2024-03-30 17:33:09 +0530

    modified movie list

--- DisneyClone/src/components/GenreMovieList.jsx
+++ DisneyClone/src/components/GenreMovieList.jsx
@@ -5,10 +5,10 @@ import MovieList from './MovieList'
 function GenreMovieList() {
   return (
     <div>
-        {GenreList.genre.map((item,index)=>index <=4 &&(
+        {GenreList.genre.map((item,index)=>index<=4&&(
             <div key={item.id} className='p-8 px-8 md:px-16' >
                 <h2 className='text-[20px] font-bold'>{item.name}</h2>
-                 <MovieList genreId={item.id}/> {/*//to iterate the scrlooing for each */}
+                 <MovieList genreId={item.id} index_={index}/> {/*//to iterate the scrlooing for each */}
             </div>
         ))}
     </div>
--- DisneyClone/src/components/MovieList.jsx
+++ DisneyClone/src/components/MovieList.jsx
@@ -2,17 +2,15 @@ import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../services/GlobalApi'
 import MovieCard from './MovieCard';
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
-// import HrMovieCard from './HrMovieCard';
-function MovieList({genreId,index_}) {
-// genreId is passed as a prop from GenreMovieList.jsx
-//use state is used to store the data in the component
+
+import PropTypes from 'prop-types';
+
+function MovieList({ genreId, }) {
+    // ...
+    // Move the entire code block inside the MovieList function
     const [movieList,setMovieList]=useState([])
     const elementRef=useRef(null);
 
-// useEffect is a hook that allows you to perform side effects in function components
-// user effect is used to fetch data from api and empty array is passed to avoid infinite loop. 
-// we can pass the array of data to fetch data on change of the array data but in this case, there will be no changes in the array so we are passing empty array.
-// The second argument of useEffect is an array of dependencies. If any of the dependencies change, the function passed to useEffect will be called.
     useEffect(()=>{
         getMovieByGenreId();
     },[])
@@ -30,28 +28,33 @@ function MovieList({genreId,index_}) {
     const slideLeft=(element)=>{
         element.scrollLeft-=500;
     }
-  return (
-    <div className='relative'>
-         <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
-         className={`text-[50px] text-white
-           p-2 z-10 cursor-pointer 
-            hidden md:block absolute
-            ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
-   
+
+    return (
+        <div className='relative'>
+    <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
+    className={`text-[50px] text-white
+    p-2 z-10 cursor-pointer 
+    hidden md:block absolute
+    mt-[150px]`}/>
+    
     <div ref={elementRef} className='flex overflow-x-auto gap-8
-     scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
-        {movieList.map((item,index)=>(
-           <>
-          {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
-           </> 
+        scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
+        {movieList.map((item) => (
+            <MovieCard key={item.id} movie={item} />
         ))}
     </div>
     <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
-           className={`text-[50px] text-white hidden md:block
-           p-2 cursor-pointer z-10 top-0
-            absolute right-0 
-            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
-    </div>
-  )
+    className={`text-[50px] text-white hidden md:block
+    p-2 cursor-pointer z-10 top-0
+    absolute right-0 
+    mt-[150px]`}/> 
+</div>
+    )
 }
+
+MovieList.propTypes = {
+    genreId: PropTypes.number.isRequired,
+    index_: PropTypes.number.isRequired,
+};
+
 export default MovieList
