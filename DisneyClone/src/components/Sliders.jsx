923856925a12a564709fed10fc6940a6e956a3a7
naflanawas <nafla.20220352@iit.ac.lk>
2024-03-30 13:15:50 +0530

    responsiveness issue sorted

--- DisneyClone/src/components/Sliders.jsx
+++ DisneyClone/src/components/Sliders.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../services/GlobalApi';
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2'; //reacticons.io
-const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original"; //baseurl from db
-// const screenWidth=window.innerWidth;
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+const screenWidth=window.innerWidth;
 function Slider() {
     const [movieList,setMovieList]=useState([]);
     const elementRef=useRef();
@@ -18,29 +18,26 @@ function Slider() {
     }
 
     const sliderRight=(element)=>{
-        const imageWidth = element.firstChild.offsetWidth;
-         element.scrollLeft += imageWidth;
+        element.scrollLeft+=screenWidth-110
     }
     const sliderLeft=(element)=>{
-        const imageWidth = element.firstChild.offsetWidth;
-        element.scrollLeft -= imageWidth;
+        element.scrollLeft-=screenWidth-110
     }
   return (
     <div>
         <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
         mx-8 mt-[150px] cursor-pointer " 
         onClick={()=>sliderLeft(elementRef.current)}/>
-        
         <HiChevronRight className='hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer right-0'  
+        mx-8 mt-[150px] cursor-pointer right-0' 
         onClick={()=>sliderRight(elementRef.current)}/>
 
    
-    <div className='flex overflow-x-auto max-w-screen w-full px-8 py-2
-    scrollbar-hide scroll-smooth' ref={elementRef}> 
+    <div className='flex overflow-x-auto w-full px-16 py-4
+    scrollbar-none scroll-smooth' ref={elementRef}> 
         {movieList.map((item, index)=>(
             <img key={index} src={IMAGE_BASE_URL+item.backdrop_path} 
-            className='w-full  flex-shrink-0 md:h-[310px] object-cover
+            className='min-w-full  md:h-[310px] object-cover
             object-left-top mr-5 rounded-md hover:border-[4px]
             border-gray-400 transition-all duration-100 ease-in'/>
         ))}
